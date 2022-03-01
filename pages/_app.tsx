17363564dd668c12d626553291aa2c4bd8c713fe
Samson Zhang <wwsamson@yahoo.com>
2022-03-01 13:29:18 -0800

    add footer and links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
 
 function MyApp({Component, pageProps}) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <div className="h-16 flex items-center px-4 fixed bottom-0 left-0 w-full bg-white z-10">
+                <p>Made by <a href="https://www.samsonzhang.com" className="underline">Samson Zhang</a> -- see <a href="https://github.com/wwsalmon/tidy-tuesday" className="underline">code on GitHub</a></p>
+            </div>
+        </>
+    );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,9 @@ export default function Home() {
         <div className="max-w-4xl mx-auto px-4 py-24">
             <div className="prose">
                 <h1>Samson's Tidy Tuesdays</h1>
-                <p>Two-hour data visualizations for the TidyTuesday project</p>
+                <p>
+                    Two-hour data visualizations for the R for Data Science <a href="https://github.com/rfordatascience/tidytuesday">#tidytuesday project</a> (but I make them in Python, D3, and React :P)
+                </p>
                 <ul>
                     <li><Link href="/2022-03-01"><a>2022-03-01: Alternative fuel stations</a></Link></li>
                 </ul>
